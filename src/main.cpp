33feb58096909dc3d1f8fa4bc89e012a047afba7
Anıl <anilgurses98@gmail.com>
2020-10-05 00:27:43 +0300

    LibIIO added for MAC and LINUX

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,34 @@
 #include "mbm-sdr/tmp.h"
 #include <iostream>
+#include <stdint.h>
+#include <string.h>
+#include <signal.h>
+#include <stdio.h>
+#include <iio/iio.h>
 
 using namespace std;
 
+/* helper macros */
+#define MHZ(x) ((long long)(x*1000000.0 + .5))
+#define GHZ(x) ((long long)(x*1000000000.0 + .5))
+
+/* common RX and TX streaming params */
+struct stream_cfg {
+	long long bw_hz; // Analog banwidth in Hz
+	long long fs_hz; // Baseband sample rate in Hz
+	long long lo_hz; // Local oscillator frequency in Hz
+	const char* rfport; // Port name
+};
+
+
+
+
+enum iodev { RX, TX };
+
 int main(){
-    cout << tmp::add(1, 2) << endl;
+    iio_scan_context *scn = iio_create_scan_context(NULL, 0);
+    struct iio_context_info** lst;
+    ssize_t n = iio_scan_context_get_info_list(scn, &lst);
+    cout << n << endl;
 }
 
